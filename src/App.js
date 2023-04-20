9713d9da8e198e7e4fa5c54456ac1a372f3b7f3f
Gauthier Pulcherie <gauthier.pulcherie@gmail.com>
2023-04-20 18:22:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import io from "socket.io-client";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const CONNECTION_URL = "wss://api.dev.stories.studio/";
+const SOCKET_PATH = "/interview-test";
+const SOCKET_TRANSPORTS = ["websocket"];
+
+const connectSocket = () => {
+  const socket = io(CONNECTION_URL, {
+    transport: SOCKET_TRANSPORTS,
+    path: SOCKET_PATH
+  });
+  // onAny subscribes to every events
+  socket.onAny((...args) => {
+    console.log(...args);
+  });
+
+  return socket;
+};
+
+const App = () => {
+  return <div>JOIN Interview </div>;
+};
 
 export default App;
